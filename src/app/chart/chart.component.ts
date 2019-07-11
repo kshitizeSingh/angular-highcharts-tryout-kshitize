226d0fdaec780e9e7e71f376ae1ca7f610434501
kshitizeSingh <kslucky12@gmail.com>
2019-07-11 15:11:53 +0530

    plot line and tool tip

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -22,7 +22,7 @@ export class ChartComponent implements OnInit {
       spend: [49.9, 71.5, -106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
       profit_i: [7.0, 8.9, 9.5, 20, 18.2, 51, -20.2, 26.5, 23.3, 18.3, 65, 9.6],
       demand_i: [7.0, 6.9, 9.5, -14.5, 15.2, -21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 10.6],
-      order_i: [7.0, 6.9, -9.5, -14.5, 15.2,-21.5, 25.2, -26.5, 23.3, 18.3, 13.9, 9.6],
+      order_i: [7.0, 6.9, -9.5, -14.5, 15.2, -21.5, 25.2, -26.5, 23.3, 18.3, 13.9, 9.6],
       roas_i: [7.0, 6.9, 9.5, -14.5, -18.2, 21.5, -27.2, 26.5, 23.3, 18.3, -13.9, 9.6],
       cpa_i: [7.0, 6.9, 9.5, -16.5, 18.2, 22.5, 25.2, 26.5, 23.3, 18.3, 19.9, 9.6],
     }
@@ -64,21 +64,25 @@ export class ChartComponent implements OnInit {
         xAxis: [{
           title: {
             text: 'spend'
-          }
+          },
+          plotLines: [{
+            color: '#FF0000', // Red
+            width: 2,
+            value: 5.5 // Position, you'll have to translate this to the values on your x axis
+          }]
         }],
-         plotLines: [{
-    color: 'red', // Color value
-    dashStyle: 'longdashdot', // Style of the plot line. Default to solid
-    value: 3, // Value of where the line will appear
-    width: 2 // Width of the line    
-  }]
+        tooltip: {
+          shared: true,
+          crosshairs: true
+        },
+
       }
 
     this.drawChart()
   }
 
   drawChart() {
-    
+
     let chart = new Chart(this.chartValue);
 
     this.chart = chart;
@@ -94,24 +98,24 @@ export class ChartComponent implements OnInit {
     if (axis == 'y1') {
 
       this.chartValue.series[0].data = this.data[this.y1]
-      this.chartValue.yAxis[0].title.text=this.y1
+      this.chartValue.yAxis[0].title.text = this.y1
     }
     if (axis == 'y2') {
       console.log(axis)
-            this.chartValue.series[1].data = this.data[this.y2]
-      this.chartValue.yAxis[1].title.text=this.y2
+      this.chartValue.series[1].data = this.data[this.y2]
+      this.chartValue.yAxis[1].title.text = this.y2
     }
     if (axis == 'y3') {
       console.log(axis)
-            this.chartValue.series[2].data = this.data[this.y3]
-      this.chartValue.yAxis[2].title.text=this.y3
+      this.chartValue.series[2].data = this.data[this.y3]
+      this.chartValue.yAxis[2].title.text = this.y3
     }
     console.log(this.chartValue.yAxis[0].title.text)
-     console.log(this.chartValue)
+    console.log(this.chartValue)
 
     this.drawChart()
   }
 
-  
+
 
 }
